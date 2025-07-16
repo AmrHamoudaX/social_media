bcec1a8d6a3f0d00277d554b620411d88bc7e871
Amr <legithamouda@gmail.com>
2025-07-16 15:59:15 +0300

    feat(User): User model to store users of the app and synchronizes it

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,5 +1,7 @@
 import { Post } from "./post.js";
+import { User } from "./user.js";
 
 Post.sync({ alter: true });
+User.sync({ alter: true });
 
-export { Post };
+export { Post, User };
---
+++ backend/models/user.js
@@ -0,0 +1,52 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../util/db.js";
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING(50),
+      unique: true,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING(64),
+      allowNull: false,
+      validate: {
+        is: /^.{8,}$/,
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    bio: {
+      type: DataTypes.TEXT,
+    },
+    profilePic: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: true,
+    modelName: "user",
+  },
+);
+
+export { User };
