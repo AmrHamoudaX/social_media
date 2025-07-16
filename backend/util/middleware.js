7913cd45045cbf8e11dc7b4ab4106c1b80ddad92
Amr <legithamouda@gmail.com>
2025-07-16 16:38:00 +0300

    feat(morgan): Get prettier colored status code and content

--- backend/util/middleware.js
+++ backend/util/middleware.js
@@ -2,11 +2,54 @@ import morgan from "morgan";
 import { loggerError } from "./logger.js";
 
 morgan.token("contentOfBody", function (req, res) {
-  return JSON.stringify(req.body);
+  const content = req.body;
+  const status = (
+    typeof res.headersSent !== `boolean`
+      ? Boolean(res._header)
+      : res.headersSent
+  )
+    ? res.statusCode
+    : undefined;
+
+  // get status color
+  const color =
+    status >= 500
+      ? 31 // red
+      : status >= 400
+        ? 33 // yellow
+        : status >= 300
+          ? 36 // cyan
+          : status >= 200
+            ? 32 // green
+            : 0; // no color
+  return `\x1b[${color}m${JSON.stringify(content)}\x1b[0m`;
+});
+
+morgan.token(`status`, (req, res) => {
+  const status = (
+    typeof res.headersSent !== `boolean`
+      ? Boolean(res._header)
+      : res.headersSent
+  )
+    ? res.statusCode
+    : undefined;
+
+  // get status color
+  const color =
+    status >= 500
+      ? 31 // red
+      : status >= 400
+        ? 33 // yellow
+        : status >= 300
+          ? 36 // cyan
+          : status >= 200
+            ? 32 // green
+            : 0; // no color
+  return `\x1b[${color}m${status}\x1b[0m`;
 });
 
 const logger = morgan(
-  ":method :url :status :res[content-length] - :response-time ms :contentOfBody",
+  ":method :url :status :res[content-length] - :response-time ms \n:contentOfBody",
 );
 
 const unknownEndpoint = (req, res) => {
