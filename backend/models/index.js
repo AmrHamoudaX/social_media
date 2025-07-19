6714bee5332fe9255ecfee9fbb4389e4a3a6530d
Amr <legithamouda@gmail.com>
2025-07-19 20:59:04 +0300

    feat: Connects teams and users using belongsToMany method method

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,12 @@
 import { Post } from "./post.js";
 import { User } from "./user.js";
+import { Team } from "./team.js";
+import { Membership } from "./membership.js";
 
 User.hasMany(Post);
 Post.belongsTo(User);
 
-export { Post, User };
+User.belongsToMany(Team, { through: Membership });
+Team.belongsToMany(User, { through: Membership });
+
+export { Post, User, Team, Membership };
