3d2f40e422615c1df90b96fd60182d661c5474be
Amr <legithamouda@gmail.com>
2025-07-21 21:02:17 +0300

    feat(login): Users login using email and password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,77 @@
-import { useEffect, useState } from "react"
-import postService from "./services/posts"
-import Post from "./components/Post"
+import { useEffect, useState } from "react";
+import postService from "./services/posts";
+import Post from "./components/Post";
+import loginService from "./services/login";
 
 function App() {
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
-  useEffect(()=> {
-    async function fetchPosts(){
-      try{
-        const allPosts = await postService.getAll()
-        setPosts(allPosts)
-      }catch(e){
-        console.error(`Error fetching posts: ${e}`)
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const allPosts = await postService.getAll();
+        setPosts(allPosts);
+      } catch (e) {
+        console.error(`Error fetching posts: ${e}`);
       }
     }
-      fetchPosts()
-  }, [])
-  return(
-  <>
-      {posts.map((post) => {
-       return <Post key={post.id} post={post} /> 
-      })}
-  </>
-  )
+    fetchPosts();
+  }, []);
 
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        email,
+        password,
+      });
+      setUser(user);
+      setEmail("");
+      setPassword("");
+    } catch {
+      setErrorMsg("Wrong credentials");
+      setTimeout(() => {
+        setErrorMsg(null);
+      }, 5000);
+    }
+  }
+
+  function loginForm() {
+    <form onSubmit={handleLogin}>
+      <div>
+        email
+        <input
+          type="text"
+          value={email}
+          name="Username"
+          onChange={({ target }) => setEmail(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>;
+  }
+
+  return (
+    <>
+      {posts.map((post) => {
+        return <Post key={post.id} post={post} />;
+      })}
+    </>
+  );
 }
 
-export default App
+export default App;
