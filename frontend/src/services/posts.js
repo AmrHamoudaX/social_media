dc4985ce0ab22d86b691cb76ed57ecb06bd233f0
Amr <legithamouda@gmail.com>
2025-07-22 10:44:05 +0300

    feat(Posts): Services module for handling api interactions with posts
    -Fetching and updating posts
    -Setting the authorization token for authenticated requests.
    -Creating a new post, which requires an authorization token.

--- frontend/src/services/posts.js
+++ frontend/src/services/posts.js
@@ -1,10 +1,28 @@
 import axios from "axios";
 
 const baseUrl = "/api/posts";
+let token = null;
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
 
 async function getAll() {
-    const request = await axios.get(baseUrl);
-    return request.data;
+  const request = await axios.get(baseUrl);
+  return request.data;
 }
 
-export default { getAll };
+async function create(newObject) {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const request = await axios.post(baseUrl, newObject, config);
+  return request.data;
+}
+
+async function update(id, newObject) {
+  const request = await axios.put(`${baseUrl}/${id}`, newObject);
+  return request.data;
+}
+
+export default { getAll, create, update, setToken };
