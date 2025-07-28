16d2af015fed796f257f71ab6d920f33ae927b81
Amr <legithamouda@gmail.com>
2025-07-28 17:33:44 +0300

    feat(app): Conditional rendering based on user authentication
    - If a user is logged in, the application displays the main feed and a
    logout button.
    - If no user is logged in, the app displays the login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import postService from "./services/posts";
 import LoginForm from "./components/LoginForm";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
@@ -24,33 +24,38 @@ function App() {
 
   return (
     <Router>
-      <div>
-        <Link style={padding} to="/login">
-          {" "}
-          Login{" "}
-        </Link>
-        <Link style={padding} to="/feed">
-          {" "}
-          Feed{" "}
-        </Link>
-      </div>
-      <p>
-        <span className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-          {" "}
-          {user && `${user.username} logged-in successfully`}
-        </span>
-        <button
-          type="button"
-          className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-          onClick={handleLogout}
-        >
-          Log Out
-        </button>
-      </p>
-      <Routes>
-        <Route path="/login" element={<LoginForm userToLogin={user} />} />
-        <Route path="/feed" element={<Feed user={user} />} />
-      </Routes>
+      {user ? (
+        <div>
+          <div>
+            <Link style={padding} to="/feed">
+              {" "}
+              Feed{" "}
+            </Link>
+            <Link style={padding} to="/login">
+              <button
+                type="button"
+                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                onClick={handleLogout}
+              >
+                Log Out
+              </button>
+            </Link>
+          </div>
+          <p>
+            <span className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+              {" "}
+              {user && `${user.username} logged-in successfully`}
+            </span>
+          </p>
+          <Routes>
+            <Route path="/feed" element={<Feed user={user} />} />
+          </Routes>
+        </div>
+      ) : (
+        <Routes>
+          <Route path="/login" element={<LoginForm userToLogin={user} />} />
+        </Routes>
+      )}
     </Router>
   );
 }
