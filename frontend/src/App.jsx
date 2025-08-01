77a8867512d52cc97aabeb312413a7e6f687e8ee
Amr <legithamouda@gmail.com>
2025-08-01 15:07:33 +0300

    feat(Navbar): Simple app navigation and user-related UI elements
    Centralizes the app's navigation, logo display and user interaction
    elements like the logout button and profile dropdown. It leverages
    new UI libraries (@headlessui/react and @heroicons/react) for its
    functionality and styling.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import postService from "./services/posts";
 import Home from "./components/Home";
 import { Link } from "react-router-dom";
+import Navbar from "./components/NavBar";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -23,29 +24,7 @@ function App() {
 
   return (
     <div>
-      <div>
-        <Link style={padding} to="/">
-          {" "}
-          IamLogo{" "}
-        </Link>
-        <Link
-          style={padding}
-          onClick={() => {
-            handleLogout();
-            const navigate = useNavigate();
-            navigate("/");
-          }}
-          to="/login"
-        >
-          Log Out
-        </Link>
-      </div>
-      <p>
-        <span className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-          {" "}
-          {user && `${user.username} logged-in successfully`}
-        </span>
-      </p>
+      <Navbar handleLogout={handleLogout} />
       <Home user={user} />
     </div>
   );
